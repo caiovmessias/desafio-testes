0c22c5f091d8c8234721313fb0634e74614ca11e
Caio Messias <caio.messias@gerencianet.com.br>
2022-02-22 00:33:22 +0000

    Cria teste para exibir usuário

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -5,7 +5,7 @@ import { createConnection } from 'typeorm';
 import { app } from '../../../../app';
 
 let connection: Connection;
-describe('Create User Controller', () => {
+describe('Show User Profile Controller', () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -34,13 +34,59 @@ describe('Create User Controller', () => {
     const { token } = responseToken.body;
 
     const response = await request(app)
-    .post('/api/v1/profile')
+    .get('/api/v1/profile')
     .set({
       Authorization: `Bearer ${token}`,
     });
 
-    console.log(response.body);
-
     expect(response.status).toBe(200);
-  })
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should not be able to show user with incorrect token', async () => {
+    const userEmail = 'userincorrect@mail.com';
+    const userPassword = '12345';
+    
+    await request(app).post('/api/v1/users').send({
+      name: 'User',
+      email: userEmail,
+      password: userPassword,
+    });
+
+    const responseToken = await request(app).post('/api/v1/sessions').send({
+      email: userEmail,
+      password: userPassword,
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+    .get('/api/v1/profile')
+    .set({
+      Authorization: `Bearer ${token}4569871`,
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('Should not be able to show user with token no exists', async () => {
+    const userEmail = 'incorrect@mail.com';
+    const userPassword = '12345';
+    
+    await request(app).post('/api/v1/users').send({
+      name: 'User',
+      email: userEmail,
+      password: userPassword,
+    });
+
+    const responseToken = await request(app).post('/api/v1/sessions').send({
+      email: userEmail,
+      password: userPassword,
+    });
+
+    const response = await request(app)
+    .get('/api/v1/profile')
+
+    expect(response.status).toBe(401);
+  });
 });
